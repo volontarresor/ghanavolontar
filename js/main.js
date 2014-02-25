ab4d5bbfb603a9c97f809f4868fef19ed26968f3
gust1n <joakim@dynamic-design.se>
2014-02-25 10:42:40 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+jQuery(document).ready(function($) {
+// $(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide"
+  });
+
+  $('.pop-project').popover({
+  	placement: "auto",
+  	trigger: "hover"
+  })
+// });
+	
+});
